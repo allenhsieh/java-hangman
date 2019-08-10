d470b03f835a14d50e99c23aec56276c7a8e8b5e
Allen Hsieh <allenhsiehhr@gmail.com>
2019-08-10 15:18:02 -0700

    refactor logic to account for 7 tries

--- Hangman.java
+++ Hangman.java
@@ -2,13 +2,9 @@ public class Hangman {
   public static void main(String[] args) {
     Game game = new Game("allen");
     Prompter prompter = new Prompter(game);
-    prompter.displayProgress();
-    boolean isHit = prompter.promptForGuess();
-    if (isHit) {
-      System.out.println("We got a hit!");
-    } else {
-      System.out.println("Oops missed");
+    while (game.getRemainingTries() > 0) {
+      prompter.promptForGuess();
+      prompter.displayProgress();
     }
-    prompter.displayProgress();
   }
 }
