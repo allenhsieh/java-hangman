180c0d276837f2ef302a566acd36ed8d50ee379a
Allen Hsieh <allenhsiehhr@gmail.com>
2019-08-10 14:42:41 -0700

    able to display "hits" and progress

--- Hangman.java
+++ Hangman.java
@@ -2,11 +2,13 @@ public class Hangman {
   public static void main(String[] args) {
     Game game = new Game("allen");
     Prompter prompter = new Prompter(game);
+    prompter.displayProgress();
     boolean isHit = prompter.promptForGuess();
     if (isHit) {
       System.out.println("We got a hit!");
     } else {
       System.out.println("Oops missed");
     }
+    prompter.displayProgress();
   }
 }
